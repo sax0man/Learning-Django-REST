666c02a21beee5e8dacd2b6c0ff41f424172a3d0
Tejas Kodoor <109711775+sax0man@users.noreply.github.com>
2025-01-03 11:50:04 +0530

    Added Author instance view

--- Project 1/bookreview/views.py
+++ Project 1/bookreview/views.py
@@ -9,3 +9,11 @@ from rest_framework.generics import ListAPIView
 class AuthorView(ListAPIView):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
+
+class AuthorInstanceView(generics.RetrieveAPIView):
+    """ 
+    Returns a single author. 
+    Also allows updating and deleting 
+    """
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer

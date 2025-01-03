4fdaa5a5ef59a111122dc268236d52923db34cf4
Tejas Kodoor <109711775+sax0man@users.noreply.github.com>
2025-01-03 11:39:43 +0530

    Added BookSerializer

--- Project 1/bookreview/serializers.py
+++ Project 1/bookreview/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
-from .models import Author
+from .models import Author, Book
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ('id', 'title', 'isbn','author')
 
 class AuthorSerializer(serializers.ModelSerializer):
+    books = BookSerializer(read_only=True, many=True, source="book_set")
     class Meta:
         model = Author
         fields = ('id', 'first_name', 'last_name')
+

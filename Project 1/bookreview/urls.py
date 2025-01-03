91b66080b217097ad727ff87e4a695a2b56327df
Tejas Kodoor <109711775+sax0man@users.noreply.github.com>
2025-01-03 11:36:49 +0530

    Did a bunch of fixes cause the main tutorial is garbage

--- Project 1/bookreview/urls.py
+++ Project 1/bookreview/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('books/', include('books.urls'))
+    # path('books/', include('books.urls'))
+    path('authors/', views.AuthorView.as_view()),
 ]

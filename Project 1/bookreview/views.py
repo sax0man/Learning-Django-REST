0f03e85204e5c0001fad1d734b89ad1956d462ba
Tejas Kodoor <109711775+sax0man@users.noreply.github.com>
2025-01-03 10:31:37 +0530

    Added bookreview views

--- Project 1/bookreview/views.py
+++ Project 1/bookreview/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from .models import Author
+from .serializers import AuthorSerializer
+from rest_framework.generics import ListAPIView
 
-# Create your views here.
+# Create your views here. 
+class AuthorView(ListAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer

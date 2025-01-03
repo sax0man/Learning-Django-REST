62c2ff26f60e94861d94e3785daa21dabead024b
Tejas Kodoor <109711775+sax0man@users.noreply.github.com>
2025-01-03 11:57:10 +0530

    Added index_view method to pass authors to index.html

--- Project 1/bookreview/views.py
+++ Project 1/bookreview/views.py
@@ -17,3 +17,13 @@ class AuthorInstanceView(generics.RetrieveAPIView):
     """
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
+
+def index_view(request):
+    """ 
+Ensure the user can only see their own profiles. 
+"""
+    response = {
+        'authors': Author.objects.all(),
+        # 'books': Book.objects.all(), 
+    }
+    return render(request, 'bookreview/index.html', response)

e10886d7953c5b8d197b79f0653487f8f98c0a13
Tejas Kodoor <109711775+sax0man@users.noreply.github.com>
2025-01-03 11:49:04 +0530

    Added Author instance URL

--- Project 1/bookreview/urls.py
+++ Project 1/bookreview/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('books/', include('books.urls'))
-    path('authors/', views.AuthorView.as_view()),
+    path('authors/', views.AuthorView.as_view(),name='author-list'),
+    path('authors/<int:pk>', views.AuthorInstanceView.as_view(),name='author-instance')
 ]

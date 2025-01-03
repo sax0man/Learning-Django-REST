f7677fdaee77eb8f95e1918f988f284699ecca20
Tejas Kodoor <109711775+sax0man@users.noreply.github.com>
2025-01-03 11:47:30 +0530

    Fixed BookSerializer

--- Project 1/bookreview/serializers.py
+++ Project 1/bookreview/serializers.py
@@ -2,13 +2,21 @@ from rest_framework import serializers
 from .models import Author, Book
 
 class BookSerializer(serializers.ModelSerializer):
+    search_url = serializers.SerializerMethodField('get_search_url')
+
     class Meta:
         model = Book
-        fields = ('id', 'title', 'isbn','author')
+        fields = ('id', 'title', 'isbn','author','search_url')
 
+    def get_search_url(self, obj):
+        return "http://www.isbnsearch.org/isbn/{}".format(obj.isbn)
+    
 class AuthorSerializer(serializers.ModelSerializer):
     books = BookSerializer(read_only=True, many=True, source="book_set")
+
     class Meta:
         model = Author
         fields = ('id', 'first_name', 'last_name','books')
+    
+   
 
